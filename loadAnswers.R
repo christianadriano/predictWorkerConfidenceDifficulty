bb02ac016006e65e1c3b12ababefcf3edfebf071
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-03-22 15:37:37 +0100

    Data and loading data

---
+++ loadAnswers.R
@@ -0,0 +1,37 @@
+#Load Answers into a dataframe
+#It also removes invalid input and outliers)
+
+loadAnswers<- function(fileName){
+  
+  setwd("C://Users//chris//OneDrive//Documentos//GitHub//workerConfidenceTrees//");
+  
+  data_all <- read.csv(fileName,header = TRUE,sep=",");
+  
+  dataf = data.frame(data_all);
+  #summary(dataf)
+  #First I need to look at the outliers or invalid values
+  #Invalid age and invalid years of experience
+  
+  #Remove rows with empty data
+  #dataf <- dataf [!(dataf$Worker.age==0),];
+  
+  #Remove confidence level zero (because it is associated with IDK answers)
+  #dataf <- dataf [!(dataf$Answer.confidence==0) ,];
+  #dataf <- dataf [!(dataf$Answer.confidence==3) ,];
+  
+  #Remove NO AND IDK ANSWERS
+ #dataf <- dataf [!(dataf$Answer.option=="IDK") ,];
+#  dataf <- dataf [!(dataf$Answer.option=="YES") ,];
+  
+  
+  #Remove invalid values
+  #dataf <- dataf [!dataf$Worker.age <1,];
+  #dataf <- dataf [!dataf$Worker.yearsOfExperience <1,];
+  
+  #Outliers
+  #Assuming that the youngest age to start programming is 10 years old
+  #Remove inputs for which age-YoE<5 
+  #dataf <- removeLinesColDiffSmallerThanValue(dataf,7,5,10)
+  #summary (dataf);
+  return(dataf);
+}
